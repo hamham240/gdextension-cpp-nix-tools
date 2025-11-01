ac40b019e59c8f8695a20d8740509ee02ce10d17
Hamham240 <hjaved340@gmail.com>
2025-10-31 18:28:12 -0700

    Expose patched godot-cpp

--- flake.nix
+++ flake.nix
@@ -17,8 +17,10 @@
           tag = "godot-4.4.1-stable";
           sha256 = "sha256-DW8OWbiA409qAyOkzxEYiPPRa5cH2TutIR7OuC3ZRgc=";
         };
-
-        godotCppSrcPatched = pkgs.stdenv.mkDerivation {
+      in
+      rec {
+        # A patched godot-cpp that is compatible with nix builds
+        packages.godot-cpp-patched = pkgs.stdenv.mkDerivation {
           pname = "godot-cpp-patched";
           version = "4.4.1";
           src = godotCppSrc;
@@ -29,15 +31,16 @@
             cp -r . $out
           '';
         };
-      in
-      {
+
+        # A devshell that offers a build ecosystem for gdextension plugins with nix
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ scons ];
           shellHook = ''
-            export GODOT_CPP_SRC=${godotCppSrcPatched}
-            export PYTHONPATH=${godotCppSrc}:$PYTHONPATH
+            export GODOT_CPP_SRC=${packages.godot-cpp-patched}
+            export PYTHONPATH=${packages.godot-cpp-patched}:$PYTHONPATH
           '';
         };
+
         formatter = pkgs.nixpkgs-fmt;
       }
     );
